d9143b89dde0d28ef5d33aa8ac9e87e98b31cb4a
jennyGuadalupeAH <91712665+jennyGuadalupeAH@users.noreply.github.com>
2021-11-19 14:02:36 -0600

    A20_ProblemaUno.m
    Identificar si es función polinomiales y racionales

---
+++ ProblemaUnoA20.m
@@ -0,0 +1,34 @@
+% Octave Script
+% School        : Tecnologico de Estudios Superiores de Jilotepec
+% Date          : 2021/11/19
+% Author        :Jennifer Guadalupe Angeles Hernandez
+% Matricula     :202123795
+% Version       : 1
+% Descripcion   : Utilizacion de Octave
+% Problema      : UNO
+% Funcion       : f(x)= (x+2) (x+2) 
+
+% Limpiar funciones
+clear
+
+% Paquete Symbolyc
+pkg load symbolic
+syms x
+
+% Fluncion Resolver
+x=(-10:1:10);
+fx= ((x+2).*(x-2));
+
+% Identificar  Funcion
+disp('Funcion polinomial')
+
+% Grafica
+plot(x,fx);
+
+grid on;
+% Titulo
+title('Problema uno');
+%Etiqueta x
+xlabel( 'Valor x');
+% Etiqueta y
+ylabel('f(x) ');
