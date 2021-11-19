61f6cb438105448449dcb0f95681d4f18bbab5f9
jennyGuadalupeAH <91712665+jennyGuadalupeAH@users.noreply.github.com>
2021-11-19 14:38:40 -0600

    A20_ProblemaDos.m
    Identificar funcion polimonial o racional

---
+++ ProblemaDosA20.m
@@ -0,0 +1,33 @@
+% Octave Script
+% School        : Tecnologico de Estudios Superiores de Jilotepec
+% Date          : 2021/11/19
+% Author        :Jennifer Guadalupe Angeles Hernandez
+% Matricula     :202123795
+% Version       : 1
+% Descripcion   : Utilizacion de Octave
+% Problema      : DOS
+% Funcion       : f(x)= 2x.^2+x.^4+x
+
+%Limpiador de variables
+clear 
+% Paquete Symbolic
+pkg load symbolic
+syms x
+% funcion resolver
+x=(-10:1:10);
+fx=((2.*x.^2)+(x.^4)+(x));
+
+% Identificar la funcion
+disp('Funcion Polimonial')
+
+% Graficar funcion 
+plot( x,fx, 'c', 'linewidth', 3);
+
+grid on;
+% Titulo
+title('Problema Dos')
+% Etiqueta para x
+xlabel(' valor de x')
+% Etiqueta para y
+ylabel('f(x)')
+
